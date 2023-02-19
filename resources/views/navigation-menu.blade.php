127088dd5f4d75bbed72e956cad3c3e96d0cf633
Sudip Das <sudip5428@gmail.com>
2023-02-19 21:49:18 +0530

    chart application

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('chatify') }}" :active="request()->routeIs('chatify')">
+                        {{ __('Chatify') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -139,6 +142,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{ route('chatify') }}" :active="request()->routeIs('chatify')">
+                {{ __('Chatify') }}
             </x-responsive-nav-link>
         </div>
 
